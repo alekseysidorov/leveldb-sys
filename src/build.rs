de2b7d0ba893f96d957070ecb6efd8a84acc27fb
Aleksei Sidorov <gorthauer87@yandex.ru>
2017-02-02 20:24:23 +0300

    Update link flags

--- src/build.rs
+++ src/build.rs
@@ -1,14 +1,13 @@
 use std::env;
 
 fn main() {
-
     if let Ok(lib_dir) = env::var("LEVELDB_LIB_DIR") {
 
-    	println!("cargo:rustc-flags=-L native={}", lib_dir);
+        println!("cargo:rustc-flags=-L native={}", lib_dir);
 
         let mode = match env::var_os("LEVELDB_STATIC") {
             Some(_) => "static",
-            None => "dylib"
+            None => "dylib",
         };
         println!("cargo:rustc-flags=-l {0}=leveldb", mode);
 
